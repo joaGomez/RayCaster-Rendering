687813e5f5227abe2f39c6e4a5f0be96c02cbfbf
joaGomez <joagomez@itba.edu.ar>
2023-06-29 23:38:36 -0300

    #1

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/joako/OneDrive/Escritorio/Storage/RayCaster-Rendering/Source_code
-# Build directory: C:/Users/joako/OneDrive/Escritorio/Storage/RayCaster-Rendering/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
